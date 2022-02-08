5d6c1450ce483251603c6590554cd0b9cbdd50bf
palemharshitha11@gmail.com <palemharshitha11@gmail.com>
2022-02-08 12:50:52 +0530

    Adding a new Project

---
+++ src/jspiders/dowhile.java
@@ -0,0 +1,15 @@
+package jspiders;
+
+class dowhile {
+
+	public static void main(String[] args) {
+		int i=100;	
+		do
+		{
+			System.out.println(i);
+			i=i-2;
+		}
+		while(i>=2);
+
+	}
+}
---
+++ src/jspiders/whileloop.java
@@ -0,0 +1,15 @@
+package jspiders;
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		int i=10000;
+				while(i>=2)
+				{
+					System.out.println(i);
+					i=i-2;
+				}
+
+	}
+
+}
